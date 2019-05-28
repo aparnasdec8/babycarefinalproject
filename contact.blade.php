ae6c224261f7546ea2c71ece3cc80bc731600a3e
aparnasdec8 <33921526+aparnasdec8@users.noreply.github.com>
2019-05-27 23:15:54 -0700

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,307 @@
+<!--A Design by W3layouts
+   Author: W3layout
+   Author URL: http://w3layouts.com
+   License: Creative Commons Attribution 3.0 Unported
+   License URL: http://creativecommons.org/licenses/by/3.0/
+   -->
+   <!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <title>Toys Shop an Ecommerce Category Bootstrap Responsive Web Template | Home :: w3layouts</title>
+      <!--meta tags -->
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="keywords" content="Toys Shop Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+         Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+      <script>
+         addEventListener("load", function () {
+         	setTimeout(hideURLbar, 0);
+         }, false);
+         
+         function hideURLbar() {
+         	window.scrollTo(0, 1);
+         }
+      </script>
+      <!--//meta tags ends here-->
+      <!--booststrap-->
+      <link href="home/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+      <!--//booststrap end-->
+      <!-- font-awesome icons -->
+      <link href="home/css/fontawesome-all.min.css" rel="stylesheet" type="text/css" media="all">
+      <!-- //font-awesome icons -->
+      <!--Shoping cart-->
+      <link rel="stylesheet" href="home/css/shop.css" type="text/css" />
+      <!--//Shoping cart-->
+      <!--stylesheets-->
+      <link href="home/css/style.css" rel='stylesheet' type='text/css' media="all">
+      <!--//stylesheets-->
+      <link href="home///fonts.googleapis.com/css?family=Sunflower:500,700" rel="stylesheet">
+      <link href="home///fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+   </head>
+   <body>
+      <!--headder-->
+      <div class="header-outs" id="home">
+      <div class="header-bar">
+         <div class="info-top-grid">
+            <div class="info-contact-agile">
+               <ul>
+                  <li>
+                     <span class="fas fa-phone-volume"></span>
+                     <p>+91 7510614142</p>
+                  </li>
+                  <li>
+                     <span class="fas fa-envelope"></span>
+                     <p><a href="mailto:info@example.com">babycaresupport@gmail.com</a></p>
+                  </li>
+                  <li>
+                  </li>
+               </ul>
+            </div>
+         </div>
+         <div class="container-fluid">
+            <div class="hedder-up row">
+               <div class="col-lg-3 col-md-3 logo-head">
+                  <h1><a class="navbar-brand" href="index.html">Baby-Care</a></h1>
+               </div>
+               <div class="col-lg-5 col-md-6 search-right">
+                  <form class="form-inline my-lg-0">
+                     <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                     <button class="btn" type="submit">Search</button>
+                  </form>
+               </div>
+               <div class="col-lg-4 col-md-3 right-side-cart">
+                  <div class="cart-icons">
+                     <ul>
+                        <li>
+                           <span class="far fa-heart"></span>
+                        </li>
+                        <li>
+                           <p><a href="/userhome.userhome">::Logout</a></p>
+						   </li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <?php
+        $datass=DB::select("select * from categories");
+        
+        ?>
+         <nav class="navbar navbar-expand-lg navbar-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+               <ul class="navbar-nav ">
+                  <li class="nav-item ">
+                     <a class="nav-link" href="home.home">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Products By Category
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        @isset($datass)
+    
+    @foreach($datass as $data)
+    <a class="nav-link "  value="{{$data->c_id}}"  href="{{url('categorypurchase/'.$data->c_id)}}" style="width:300px;">{{$data->category}}</a>
+    @endforeach
+    @endisset
+                           
+                        </div>
+                     </li>
+                     <li class="nav-item">
+                     <a href="/shopnow" class="nav-link">Shop Now</a>
+                  </li>
+                  <li class="nav-item">
+                     <a href="home.about" class="nav-link">About</a>
+                  </li>
+                  <li class="nav-item">
+                     <a href="home.service" class="nav-link">Service</a>
+                  </li>
+                  
+                  <li class="nav-item active">
+                     <a href="home.contact " class="nav-link">Contact</a>
+                  </li>
+               </ul>
+            </div>
+         </nav>
+      </div>
+	  </div>
+      <!--//headder-->
+      <!-- banner -->
+      <div class="inner_page-banner one-img">
+      </div>
+      <!--//banner -->
+      <!-- short -->
+      <div class="using-border py-3">
+         <div class="inner_breadcrumb  ml-4">
+            <ul class="short_ls">
+               <li>
+                  <a href="home.home">Home</a>
+                  <span>/ /</span>
+               </li>
+               <li>Contact</li>
+            </ul>
+         </div>
+      </div>
+      <!-- //short-->
+      <!--contact -->
+      <section class="contact py-lg-4 py-md-3 py-sm-3 py-3">
+         <div class="container py-lg-5 py-md-4 py-sm-4 py-3">
+            <h3 class="title text-center mb-lg-5 mb-md-4 mb-sm-4 mb-3">Contact US</h3>
+            <div class="contact-list-grid">
+               <form action="#" method="post">
+                  <div class=" agile-wls-contact-mid">
+                     <div class="form-group contact-forms">
+                        <input type="text" class="form-control" placeholder="Name">
+                     </div>
+                     <div class="form-group contact-forms">
+                        <input type="email" class="form-control" placeholder="Enter">
+                     </div>
+                     <div class="form-group contact-forms">
+                        <input type="text" class="form-control" placeholder="Phone">
+                     </div>
+                     <div class="form-group contact-forms">
+                        <textarea class="form-control" rows="3"></textarea>
+                     </div>
+                     <button type="submit" class="btn btn-block sent-butnn">Send</button>
+                  </div>
+               </form>
+            </div>
+         </div>
+         <!--//contact-map -->
+      </section>
+      <!--subscribe-address-->
+      
+      <!--//subscribe-address-->
+      <section class="sub-below-address py-lg-4 py-md-3 py-sm-3 py-3">
+         <div class="container py-lg-5 py-md-5 py-sm-4 py-3">
+            <h3 class="title clr text-center mb-lg-5 mb-md-4 mb-sm-4 mb-3">Get In Touch Us</h3>
+            <div class="icons mt-4 text-center">
+               <ul>
+                  <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+                  <li><a href="#"><span class="fas fa-envelope"></span></a></li>
+                  <li><a href="#"><span class="fas fa-rss"></span></a></li>
+                  <li><a href="#"><span class="fab fa-vk"></span></a></li>
+               </ul>
+               <p class="my-3">velit sagittis vehicula. Duis posuere 
+                  ex in mollis iaculis. Suspendisse tincidunt
+                  velit sagittis vehicula. Duis posuere 
+                  velit sagittis vehicula. Duis posuere 
+               </p>
+            </div>
+            <div class="email-sub-agile">
+               <form action="#" method="post">
+                  <div class="form-group sub-info-mail">
+                     <input type="email" class="form-control email-sub-agile" placeholder="Email">
+                  </div>
+                  <div class="text-center">
+                     <button type="submit" class="btn subscrib-btnn">Subscribe</button>
+                  </div>
+               </form>
+            </div>
+         </div>
+      </section>
+      <!--//subscribe-->
+      <!-- footer -->
+      <footer class="py-lg-4 py-md-3 py-sm-3 py-3 text-center">
+         <div class="copy-agile-right">
+            <p> 
+               © 2019 Babycare. All Rights Reserved | Design by <a href="http://www.W3Layouts.com" target="_blank">W3Layouts</a>
+            </p>
+         </div>
+      </footer>
+      <!-- //footer -->
+      <!-- Modal 1-->
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+         <div class="modal-dialog" role="document">
+            <div class="modal-content">
+               <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                  </button>
+               </div>
+               <div class="modal-body">
+                  <div class="register-form">
+                     <form action="#" method="post">
+                        <div class="fields-grid">
+                           <div class="styled-input">
+                              <input type="text" placeholder="Your Name" name="Your Name" required="">
+                           </div>
+                           <div class="styled-input">
+                              <input type="email" placeholder="Your Email" name="Your Email" required="">
+                           </div>
+                           <div class="styled-input">
+                              <input type="password" placeholder="password" name="password" required="">
+                           </div>
+                           <button type="submit" class="btn subscrib-btnn">Login</button>
+                        </div>
+                     </form>
+                  </div>
+               </div>
+               <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- //Modal 1-->
+      <!--js working-->
+      <script src='home/js/jquery-2.2.3.min.js'></script>
+      <!--//js working-->
+      <!-- cart-js -->
+      <script src="home/js/minicart.js"></script>
+      <script>
+         toys.render();
+         
+         toys.cart.on('toys_checkout', function (evt) {
+         	var items, len, i;
+         
+         	if (this.subtotal() > 0) {
+         		items = this.items();
+         
+         		for (i = 0, len = items.length; i < len; i++) {}
+         	}
+         });
+      </script>
+      <!-- //cart-js -->
+      <!-- start-smoth-scrolling -->
+      <script src="home/js/move-top.js"></script>
+      <script src="home/js/easing.js"></script>
+      <script>
+         jQuery(document).ready(function ($) {
+         	$(".scroll").click(function (event) {
+         		event.preventDefault();
+         		$('html,body').animate({
+         			scrollTop: $(this.hash).offset().top
+         		}, 900);
+         	});
+         });
+      </script>
+      <!-- start-smoth-scrolling -->
+      <!-- here stars scrolling icon -->
+      <script>
+         $(document).ready(function () {
+         
+         	var defaults = {
+         		containerID: 'toTop', // fading element id
+         		containerHoverID: 'toTopHover', // fading element hover id
+         		scrollSpeed: 1200,
+         		easingType: 'linear'
+         	};
+         
+         
+         	$().UItoTop({
+         		easingType: 'easeOutQuart'
+         	});
+         
+         });
+      </script>
+      <!-- //here ends scrolling icon -->
+      <!--bootstrap working-->
+      <script src="home/js/bootstrap.min.js"></script>
+      <!-- //bootstrap working-->      <!-- //OnScroll-Number-Increase-JavaScript -->
+   </body>
+</html>
